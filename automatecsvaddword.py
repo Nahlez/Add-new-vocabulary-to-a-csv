c2e38640e06fb278b60755fb90f4a80f4fcffe00
Nhlez <notrealmail@notrealmail.ps.com.ps>
2021-09-21 22:56:53 +0200

    reverted some changes

--- automatecsvaddword.py
+++ automatecsvaddword.py
@@ -3,17 +3,22 @@ import pyperclip
 import csv
 import time
 
+shortcut = 'c+w' #If you want to change the shortcut just replace "c+w" for your pair of  favourite letters
+
 while True:
-    time.sleep(0.1)
-    if keyboard.is_pressed('a+w'): #a + w come from "add word"
-        time.sleep(0.1)
-        save_content = str(pyperclip.paste())
-        vocab_csv = open('vocab.csv', 'a+')
-        print('The word',save_content, 'was added')
-        vocab_csv.write(save_content.title()+'\n')
-        vocab_csv.close()
-    
-    else:
-        continue
+    try:
+        if keyboard.is_pressed(shortcut): # c + w come from "copy word"
+            save_content = str(pyperclip.paste()).title()
+            vocab_csv = open('vocab.csv', 'a+')
+            print('"'+save_content+'"', 'was added.')
+            vocab_csv.write(save_content+'\n')
+            vocab_csv.close()
+            time.sleep(0.1)
+    except Exception as e:
+        print(e)
+
+        
+            
 
 # No añadir duplicados
+# Not i did this way because it can works with any text in your laptop. Check if it works in linux
